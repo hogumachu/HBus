5978fe7f8a312ce0cd793d3d2ecb7ea7fd64190b
hogumachu <castle_jun@daum.net>
2021-12-20 16:28:28 +0900

    init

---
+++ HBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HBus
+//
+//  Created by 홍성준 on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
