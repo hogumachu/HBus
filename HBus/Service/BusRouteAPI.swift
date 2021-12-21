53f514d4507f3a2a9e3f71ca2d2fb72d49aee3bc
hogumachu <castle_jun@daum.net>
2021-12-21 11:35:46 +0900

    Custom DispatchQueue

--- HBus/Service/BusRouteAPI.swift
+++ HBus/Service/BusRouteAPI.swift
@@ -5,7 +5,7 @@ class BusRouteAPI {
     private init() {}
     
     func getBusRouteList(routeName: String, completion: @escaping (Result<[BusRoute], Error>) -> ()) {
-        DispatchQueue.global().async {
+        DispatchQueue(label: "getBusRouteList").async {
             let urlString = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteList?serviceKey=\(_serviceKey)&keyword=\(routeName)"
             let url = URL(string: urlString)!
             
@@ -21,7 +21,7 @@ class BusRouteAPI {
     }
     
     func getBusRouteStationList(routeID: String, completion: @escaping (Result<[BusRouteStation], Error>) -> ()) {
-        DispatchQueue.global().async {
+        DispatchQueue(label: "getBusRouteStationList").async {
             let urlString = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteStationList?serviceKey=\(_serviceKey)&routeId=\(routeID)"
             let url = URL(string: urlString)!
             
@@ -37,7 +37,7 @@ class BusRouteAPI {
     }
     
     func getBusLocationList(routeID: String, completion: @escaping (Result<[BusLocation], Error>) -> ()) {
-        DispatchQueue.global().async {
+        DispatchQueue(label: "getBusLocationList").async {
             let urlString = "http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList?serviceKey=\(_serviceKey)&routeId=\(routeID)"
             let url = URL(string: urlString)!
             
@@ -53,21 +53,23 @@ class BusRouteAPI {
     }
     
     func getBusRouteInfoItem(routeID: String, completion: @escaping (Result<[BusRoute], Error>) -> ()) {
-        let urlString = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteInfoItem?serviceKey=\(_serviceKey)&routeId=\(routeID)"
-        let url = URL(string: urlString)!
-        
-        BusRouteInfoXMLParseHepler.shared.parse(url) { result in
-            switch result {
-            case .success(let busRouteList):
-                completion(.success(busRouteList))
-            case .failure(let error):
-                completion(.failure(error))
+        DispatchQueue(label: "getBusRouteInfoItem").async {
+            let urlString = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteInfoItem?serviceKey=\(_serviceKey)&routeId=\(routeID)"
+            let url = URL(string: urlString)!
+            
+            BusRouteInfoXMLParseHepler.shared.parse(url) { result in
+                switch result {
+                case .success(let busRouteList):
+                    completion(.success(busRouteList))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
             }
         }
     }
     
     func getBusStationViaRouteList(stationID: String, completion: @escaping (Result<[BusRoute], Error>) -> ()) {
-        DispatchQueue.global().async {
+        DispatchQueue(label: "getBusStationViaRouteList").async {
             let urlString = "http://apis.data.go.kr/6410000/busstationservice/getBusStationViaRouteList?serviceKey=\(_serviceKey)&stationId=\(stationID)"
             let url = URL(string: urlString)!
             
